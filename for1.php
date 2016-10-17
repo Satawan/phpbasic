5f8582ef6a6cc260140f9aca687f098484bf3eae
satawan2003 <satawan2003@yahoo.com>
2016-10-17 14:23:59 +0700

    phpbasic project

---
+++ for1.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+for($x=0;$x<=15;$x++){
+    
+    echo $x.'<hr>';
+    
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+            <meta charset="x-windows-874">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $a='Hello PHP';
+        $b=20;
+        $c=233.34;
+        $d=true;
+        $e=array("apple","papaya","orange");
+        echo $a.'/'.$b.'/'.$c.'/'.$d;
+        echo'<br>';
+        print_r($e);
+        echo $e[1];
+        
+        // put your code here
+        ?>
+    </body>
+</html>
